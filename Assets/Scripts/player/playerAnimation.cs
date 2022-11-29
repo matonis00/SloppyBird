9d27e4d59c521c4874b54dd0d3c0ab2cf0126403
Mateusz Cudzik <mattcudzikpl@gmail.com>
2022-11-29 23:33:28 +0100

    zmiany

---
+++ Assets/Scripts/player/playerAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour
+{
+    playerEventController eventController;
+    Animator animator;
+    
+    void Start()
+    {
+        eventController = gameObject.GetComponent<playerEventController>();
+        animator = gameObject.GetComponent<Animator>();
+
+        eventController.onJumpEvent.AddListener(onJump);
+        eventController.onFallEvent.AddListener(onFall);
+        eventController.onDeath.AddListener(onDeath);
+    }
+
+    private void onJump()
+    {
+        animator.SetBool("jumping", true);
+    }
+    private void onDeath()
+    {
+        animator.SetTrigger("hit");
+    }
+    private void onFall()
+    {
+        animator.SetBool("jumping", false);
+    }
+}
